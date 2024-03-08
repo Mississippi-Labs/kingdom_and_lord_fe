94db03e30b77aa1563313ebadf0f365cff3f090a
Tiyo <tiyosheng@gmail.com>
2024-03-08 14:20:05 +0800

    x

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import wasm from "vite-plugin-wasm";
 import vueJsx from '@vitejs/plugin-vue-jsx';
-// import topLevelAwait from "vite-plugin-top-level-await";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(), vueJsx()],
+  plugins: [vue(), wasm(), vueJsx(), topLevelAwait()],
 })
