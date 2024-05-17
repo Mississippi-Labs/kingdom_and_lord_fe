44d47577a85afcee2498f253e33cff0138c4f332
Tiyo <tiyosheng@gmail.com>
2024-05-17 22:59:04 +0800

    del code

--- src/components/dialog/UnderUpgradingDialog.vue
+++ src/components/dialog/UnderUpgradingDialog.vue
@@ -38,7 +38,6 @@ const isDisabled = (resource, amount) => {
     return true
   }
   resource.forEach(item => {
-    console.log(item.count * amount, props.resourceData, [item])
     if (item.count * amount > props.resourceData[item.name]) {
       disabled = true
     }
